28c432b6adca50f8ce9f96bf312714ca507893a6
djsavvy <github.com/djsavvy>
2018-09-02 13:34:05 -0400

    Working configs on new laptop

--- .config/i3/SetWallpaper.sh
+++ .config/i3/SetWallpaper.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-LOCKIMAGE=/tmp/i3lock.png
-WALLPAPER=/tmp/i3wall.png
-icon=~/.config/i3/lock/icons/computer_locked.png
+LOCKIMAGE="/tmp/i3lock.png"
+WALLPAPER="/tmp/i3wall.png"
+icon="$HOME/.config/i3/lock/icons/computer_locked.png"
 
+# If wallpaper changes on each login, then this can be commented out
+# Since it makes for a harsh transition from the old wallpaper to the new one
+# Caching optimizes for the case where the wallpaper isn't actually changing -- provides a faster startup
+if [ -f $WALLPAPER ]; then
+    feh --bg-scale $WALLPAPER &
+fi
 
 convert $1 -resize 1920x1080\! $WALLPAPER
 feh --bg-scale $WALLPAPER &
