62b370a9c04bd24dc6588fee1b01e87369149309
djsavvy <github.com/djsavvy>
2018-09-03 20:31:18 -0400

    Updated music workspace switching

--- .config/polybar/musicWorkspaceSwitcher.sh
+++ .config/polybar/musicWorkspaceSwitcher.sh
@@ -3,12 +3,12 @@
 curWorkpace=`i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2`
 
 notify-send $curWorkspace
-notify-send $curWorkspace -eq 
+notify-send $curWorkspace -eq 11:
 
-if [[ "$curWorkpace" == "" ]]; 
+if [[ "$curWorkpace" == "11:" ]]; 
     then 
         i3-msg workspace back_and_forth
     else 
-        i3-msg workspace 
+        i3-msg workspace 11:
 fi
 
