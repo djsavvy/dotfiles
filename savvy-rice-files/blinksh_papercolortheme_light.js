56adf5863a3c54731e80a66e509be3eeb209667e
djsavvy <sraghuvanshi@college.harvard.edu>
2020-04-15 18:18:26 -0500

    blinksh_papercolortheme_light: Make cursor darker

--- savvy-rice-files/blinksh_papercolortheme_light.js
+++ savvy-rice-files/blinksh_papercolortheme_light.js
@@ -1,4 +1,4 @@
 t.prefs_.set('color-palette-overrides',["#f8f8f8", "#d7005f", "#718c00", "#ff8f01", "#005f87", "#6a1e9a", "#3e999f", "#2c2c2c", "#2c2c2c", "#ef5251", "#90c94d", "#ffa000", "#4271ae", "#c774c1", "#3e999f", "#f5f5f5"]);
 t.prefs_.set('foreground-color', "#3c3c3b");
 t.prefs_.set('background-color', "#eeeeee");
-t.prefs_.set('cursor-color', 'rgba(224, 224, 224, 0.8)')
+t.prefs_.set('cursor-color', 'rgba(244, 244, 244, 0.7)')
