a216af310b1f6c9e9043fa817c0df526d9748e77
djsavvy <sraghuvanshi@college.harvard.edu>
2020-04-15 17:59:17 -0500

    Add transparency to blinksh_papercolortheme_light

--- savvy-rice-files/blinksh_papercolortheme_light.js
+++ savvy-rice-files/blinksh_papercolortheme_light.js
@@ -1,4 +1,5 @@
 t.prefs_.set('color-palette-overrides',["#f8f8f8", "#d7005f", "#718c00", "#ff8f01", "#005f87", "#6a1e9a", "#3e999f", "#2c2c2c", "#2c2c2c", "#ef5251", "#90c94d", "#ffa000", "#4271ae", "#c774c1", "#3e999f", "#f5f5f5"]);
 t.prefs_.set('foreground-color', "#3c3c3b");
 t.prefs_.set('background-color', "#eeeeee");
-t.prefs_.set('cursor-color', "#d0d0d0");
+// t.prefs_.set('cursor-color', "#d0d0d0");
+t.prefs_.set('cursor-color', rgba(224, 224, 224, 0.5))
