f77278ec7ed1cbc0d12f92598e52f009cb649241
djsavvy <sraghuvanshi@college.harvard.edu>
2021-05-30 21:23:46 -0400

    Update Powershell Profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,5 +3,17 @@
 Set-PSReadLineOption -BellStyle None -EditMode Emacs
 Set-PSReadLineKeyHandler -Chord ctrl+w -Function BackwardDeleteWord
 
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
+
 function neovide { neovide-0.7.0.exe --multiGrid $args }
-function vim { neovide $args }
+function vim { nvim $args }
+
+Import-Module posh-git
+
+$ENV:STARSHIP_CONFIG = "$HOME\.starship"
+$ENV:RIPGREP_CONFIG_PATH = "\\wsl$\Arch\home\savvy\dotfiles\.config\.ripgreprc"
+Invoke-Expression (&starship init powershell)
