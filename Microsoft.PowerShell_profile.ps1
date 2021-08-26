51dade0ea1b95da78e9a9536c205f541f8edb649
djsavvy <sraghuvanshi@college.harvard.edu>
2021-08-25 21:10:01 -0500

    Backup windows files from g7

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,7 +10,40 @@ Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
 function neovide { neovide-0.7.0.exe --multiGrid $args }
-function vim { nvim $args }
+function vim { 
+  if ((Test-Path Env:\TERM_PROGRAM) -and ("vscode" -eq (Get-Item -Path Env:\TERM_PROGRAM).Value)) {
+    if (-not ($args.Count -eq 0)) {
+      code $args
+    }
+  }
+  else {
+    nvim $args 
+  }
+}
+
+function :e { vim $args }
+function :E { vim $args }
+function :q { exit }
+function :Q { exit }
+function exi { exit }
+
+# Git aliases
+function g { git $args }
+function gi { git $args }
+function gpul { git pull $args }
+function gpull { git pull $args }
+function gc { git checkout $args }
+function gcp { git cherry-pick $args }
+function gr { git rebase $args }
+function gri { git rebase -i $args }
+function gd { git diff $args }
+function gds { git diff --staged $args }
+function ga { git add $args }
+function gap { git add -p $args }
+Remove-Alias -Name gcm -Force
+function gcm { git commit -m $args }
+function gstat { git status $args }
+function gpush { git push $args }
 
 Import-Module posh-git
 
