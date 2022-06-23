834ed056938af8443d3dd07a6d1c99803c5e0507
Savvy Raghuvanshi <sraghuvanshi@faralloncapital.com>
2022-06-23 08:33:54 -0700

    Add new git aliases to Powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,7 +9,6 @@ Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
-function neovide { neovide-0.7.0.exe --multiGrid $args }
 function vim { 
   if ((Test-Path Env:\TERM_PROGRAM) -and ("vscode" -eq (Get-Item -Path Env:\TERM_PROGRAM).Value)) {
     if (-not ($args.Count -eq 0)) {
@@ -20,6 +19,7 @@ function vim {
     nvim $args 
   }
 }
+function vi { vim $args }
 
 function :e { vim $args }
 function :E { vim $args }
@@ -32,7 +32,11 @@ function g { git $args }
 function gi { git $args }
 function gpul { git pull $args }
 function gpull { git pull $args }
+function gpush { git push $args }
+function gpf { git push --force $args }
+function gpuo { git push -u origin "$(git branch --show-current)" }
 function gc { git checkout $args }
+function gcb { git checkout -b $args }
 function gcp { git cherry-pick $args }
 function gr { git rebase $args }
 function gri { git rebase -i $args }
@@ -40,10 +44,25 @@ function gd { git diff $args }
 function gds { git diff --staged $args }
 function ga { git add $args }
 function gap { git add -p $args }
+function ga. { git add . $args }
 Remove-Alias -Name gcm -Force
 function gcm { git commit -m $args }
 function gstat { git status $args }
+function gca { git commit --amend $args }
+function gs { git stash $args }
+function gssp { git stash show -p $args }
+function gsd { git stash drop $args }
+function gsp { git show -p $args }
+function gf { git fetch $args }
 function gpush { git push $args }
+
+function cd.. { cd .. }
+function .. { cd .. }
+function ... { cd ../.. }
+function .... { cd ../../..}
+
+function ll { Get-ChildItem -Force $args }
+
 
 Import-Module posh-git
 
